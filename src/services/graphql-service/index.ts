16e96b1c6811e9fea7b20ed93e9ed4e18e5772b8
Semih <semih9569@gmail.com>
2023-09-13 14:31:04 +0300

    chore: give a path name to apollo server

--- src/services/graphql-service/index.ts
+++ src/services/graphql-service/index.ts
@@ -4,11 +4,8 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from './graphql/schema';
 import resolvers from './graphql/resolvers';
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
 (async () => {
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  console.log(`🚀  Server ready at: ${url}`);
+  const server = new ApolloServer({ typeDefs, resolvers });
+  const { url } = await startStandaloneServer(server, { listen: { port: 4000, path: '/graphql' } });
+  console.log(`🚀 Server is ready at: ${url}`);
 })();
