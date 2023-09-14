5f711e6c3b7c4a8ddbcc493ff2f9767d50bcd6f7
Semih <semih9569@gmail.com>
2023-09-14 13:14:03 +0300

    fix: remove unnecessary packages from package.json

--- src/services/data-pulling-service/index.ts
+++ src/services/data-pulling-service/index.ts
@@ -4,4 +4,5 @@ import { OpenChargeMapService } from './api';
 import { DataFetcher } from './fetch-data.service';
 
 const dataFetcher = new DataFetcher(new OpenChargeMapService(), DatabaseService.getInstance());
-setInterval(() => dataFetcher.fetchData(), DATA_PULLING_INTERVAL / 20);
+dataFetcher.fetchData();
+setInterval(() => dataFetcher.fetchData(), DATA_PULLING_INTERVAL);
